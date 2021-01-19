cf152ecb1f9c0a3cf1462e2bee2a0fd6d64a420a
Dima Matroskin <dima.matroskin062@gmail.com>
2021-01-19 02:41:12 +0200

    new block started

---
+++ js/news.js
@@ -0,0 +1,32 @@
+const news = () => {
+    const container = document.querySelector('.news div');
+    container.innerHTML = '';
+    NEWS.map((el) => {
+        container.innerHTML += `
+        <hr>
+        <article>
+                    <div class="news-img">
+                        <div class="img" id="news_1"></div>
+                        <div class="news-day">${parseDate(el.date).day}</div>
+                        <p class="news-mounts">${parseDate(el.date).month}</p>
+                    </div>
+                    <div class="news-txt">
+                        <a href=${el.url}>${el.title}</a><br />${el.description}
+                    </div>
+                </article>`;
+    });
+};
+
+function parseDate(date) {
+    const parsedDate = new Date(date)
+        .toLocaleString('ru-RU', {
+            day: 'numeric',
+            month: 'long',
+        })
+        .split(' ');
+    return {
+        day: parsedDate[0],
+        month: parsedDate[1],
+    };
+}
+news();
