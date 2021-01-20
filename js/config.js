eeb4aa96d278372e581667bfb8a53e47757edae6
Dima Matroskin <dima.matroskin062@gmail.com>
2021-01-20 11:37:22 +0200

    styling for footer added

--- js/config.js
+++ js/config.js
@@ -16,21 +16,16 @@ const TOP_MENU = {
     title: 'Каталог',
     submenu: [
       {
-        order: 2,
-        title: 'GSM оборудование',
-        url: 'gsm_equipment.html',
-      },
-      {
         order: 1,
         title: 'VOIP оборудование',
         url: 'voip_equipment.html',
       },
+      {
+        order: 2,
+        title: 'GSM оборудование',
+        url: 'gsm_equipment.html',
+      },
     ],
-  },
-  payment_delivery: {
-    order: 3,
-    title: 'Payment and delivery',
-    url: 'payment_delivery.html',
   },
   about_company: {
     order: 2,
@@ -47,6 +42,11 @@ const TOP_MENU = {
         url: 'gsm_equipment.html',
       },
     ],
+  },
+  payment_delivery: {
+    order: 3,
+    title: 'Payment and delivery',
+    url: 'payment_delivery.html',
   },
 };
 
@@ -72,9 +72,19 @@ const MENU = [
     url: 'video_equipment.html',
   },
   {
-    order: 4,
-    title: 'VIDEO оборудование',
-    url: 'video_equipment.html',
+    order: 1,
+    title: 'VOIP ОБОРУДОВАНИЕ',
+    url: 'voip_equipment.html',
+  },
+  {
+    order: 2,
+    title: 'SKYPE оборудование',
+    url: 'skype_equipment.html',
+  },
+  {
+    order: 3,
+    title: 'GSM оборудование',
+    url: 'gsm_equipment.html',
   },
   {
     order: 4,
@@ -82,9 +92,19 @@ const MENU = [
     url: 'video_equipment.html',
   },
   {
-    order: 4,
-    title: 'VIDEO оборудование',
-    url: 'video_equipment.html',
+    order: 1,
+    title: 'VOIP ОБОРУДОВАНИЕ',
+    url: 'voip_equipment.html',
+  },
+  {
+    order: 2,
+    title: 'SKYPE оборудование',
+    url: 'skype_equipment.html',
+  },
+  {
+    order: 3,
+    title: 'GSM оборудование',
+    url: 'gsm_equipment.html',
   },
   {
     order: 4,
@@ -92,9 +112,19 @@ const MENU = [
     url: 'video_equipment.html',
   },
   {
-    order: 4,
-    title: 'VIDEO оборудование',
-    url: 'video_equipment.html',
+    order: 1,
+    title: 'VOIP ОБОРУДОВАНИЕ',
+    url: 'voip_equipment.html',
+  },
+  {
+    order: 2,
+    title: 'SKYPE оборудование',
+    url: 'skype_equipment.html',
+  },
+  {
+    order: 3,
+    title: 'GSM оборудование',
+    url: 'gsm_equipment.html',
   },
   {
     order: 4,
@@ -109,28 +139,21 @@ const NEWS = [
     title: 'Новинка от «Элтекс» - точка доступа WEP',
     description:
       'Предприятие «Элтекс» запустило производство точки доступа WEP-2ac.',
-    img: '/img/news_1.png',
+    img: 'https://same_url.jpg',
     url: 'voip_equipment.html',
   },
   {
     date: '2020/12/25',
     title: 'Новинка от компании Grandstream!',
     description: 'Скоро в продаже мощный маршрутизатор GWN7000!',
-    img: '/img/news_2.png',
-    url: 'voip_equipment.html',
-  },
-  {
-    date: '2021/01/14',
-    title: 'WiFi точка sдоступа от компании Grandstream!',
-    description: 'Уже скоро в продаже!',
-    img: '/img/news_3.png',
+    img: 'https://same_url.jpg',
     url: 'voip_equipment.html',
   },
   {
     date: '2021/01/14',
     title: 'WiFi точка доступа от компании Grandstream!',
     description: 'Уже скоро в продаже!',
-    img: '/img/news_1.jpg',
+    img: 'https://same_url.jpg',
     url: 'voip_equipment.html',
   },
 ];
@@ -138,48 +161,48 @@ const NEWS = [
 const BANNER = [
   {
     order: 1,
-    img: '/img/slider.png',
+    img: 'https://same_url.jpg',
     url: 'voip_equipment.html',
   },
   {
     order: 2,
-    img: 'img/slider.png',
-    url: 'slider.html',
+    img: 'https://same_url2.jpg',
+    url: 'voip_equipment.html',
   },
 ];
 
 // type ['new', 'recommended', 'sale']
 const ITEMS = [
-  // {
-  //   type: 'new',
-  //   description: 'IP телефон Siemens Gigaset C530A IP',
-  //   img: '/img/tel_1.png',
-  //   // price: '1000',
-  //   oldPrice: '1100',
-  //   currency: 'UAH',
-  //   date: '2021/01/01',
-  //   url: 'new_items/item1.html',
-  // },
-  // {
-  //   type: 'recommended',
-  //   description: '6IP телефон Siemens Gigaset C530A IP',
-  //   img: '/img/tel_3.png',
-  //   price: '5000',
-  //   oldPrice: '1100',
-  //   currency: 'RUB',
-  //   date: '2020/12/25',
-  //   url: 'new_items/item.html',
-  // },
-  // {
-  //   type: 'sale',
-  //   description: '8IP телефон Siemens Gigaset C530A IP',
-  //   // img: 'https://same',
-  //   price: '150',
-  //   oldPrice: '200',
-  //   currency: 'USD',
-  //   date: '2021/01/13',
-  //   url: 'https://same_url/item.html',
-  // },
+  {
+    type: 'new',
+    description: 'IP телефон Siemens Gigaset C530A IP',
+    img: 'https://same_url.jpg',
+    price: '1000',
+    oldPrice: '1100',
+    currency: 'UAH',
+    date: '2021/01/01',
+    url: 'new_items/item1.html',
+  },
+  {
+    type: 'recommended',
+    description: 'IP телефон Siemens Gigaset C530A IP',
+    img: 'https://same_url.jpg',
+    price: '300',
+    oldPrice: '1100',
+    currency: 'RUB',
+    date: '2020/12/25',
+    url: 'new_items/item.html',
+  },
+  {
+    type: 'sale',
+    description: 'IP телефон Siemens Gigaset C530A IP',
+    img: 'https://same_url.jpg',
+    price: '150',
+    oldPrice: '200',
+    currency: 'USD',
+    date: '2021/01/13',
+    url: 'https://same_url/item.html',
+  },
 ];
 
 // time_action format: "d" - day, "h" - hour, "m" - minute. if doesn't exist = infinity
@@ -195,43 +218,13 @@ const PROMOTIONS = [
     title: 'Название акции 2',
     description: 'IP телефон Siemens Gigaset C530A IP',
     img: 'https://same_url.jpg',
-    // url: 'https://same_url/item.html',
+    url: 'https://same_url/item.html',
   },
 ];
 
 const BUYING_RIGHT_NOW = [
   {
     title: 'Название товара',
-    // img: 'https://same_url.jpg',
-    url: 'https://same_url/item.html',
-  },
-  {
-    title: 'Название товара 2',
-    img: 'https://same_url.jpg',
-    url: 'https://same_url/item.html',
-  },
-  {
-    title: 'Название товара',
-    img: 'https://same_url.jpg',
-    url: 'https://same_url/item.html',
-  },
-  {
-    title: 'Название товара 2',
-    img: 'https://same_url.jpg',
-    url: 'https://same_url/item.html',
-  },
-  {
-    title: 'Название товара 2',
-    img: 'https://same_url.jpg',
-    url: 'https://same_url/item.html',
-  },
-  {
-    title: 'Название товара 2',
-    img: 'https://same_url.jpg',
-    url: 'https://same_url/item.html',
-  },
-  {
-    title: 'Название товара 2',
     img: 'https://same_url.jpg',
     url: 'https://same_url/item.html',
   },
